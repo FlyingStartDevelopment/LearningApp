92bce63d3ec4422302d5272e04bf180602ed7e5c
Karrin Burns <karrinburns@gmail.com>
2021-06-08 09:26:03 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Karrin Burns on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
